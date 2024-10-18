4c2b8ef80a555848bf5089b5cf9a68e59b24e477
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-10-18 00:21:23 -0700

    Makeup website project files

--- assignment-03/src/components/HeroSection.tsx
+++ assignment-03/src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="text-center">
                 <h1 className="text-6xl font-bold text-pink-600 mb-4">Unleash Your Beauty</h1>
-                <p className="text-lg text-pink-600 mb-8 font-semibold">Enhancing Your Look with Professional Makeup Services</p>
+                <p className="text-4xl text-pink-600 mb-8 font-semibold">Enhancing Your Look with Professional Makeup Services</p>
                 <Link href="#services">
                     <button className="bg-pink-600 hover:bg-pink-700 text-white py-2 px-4 rounded-lg transition duration-300">
                         Explore Services
