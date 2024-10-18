48a7c60a9df8cd3f2a61a22007c5cd122566dea9
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-10-18 03:05:07 -0700

    Update HeroSection.tsx

--- assignment-03/src/components/HeroSection.tsx
+++ assignment-03/src/components/HeroSection.tsx
@@ -9,8 +9,7 @@ const HeroSection = () => {
                 backgroundImage: "url('/hero-img.jpg')"
             }}
         >
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="text-center">
+            className="text-center">
                 <h1 className="text-6xl font-bold text-pink-600 mb-4">Unleash Your Beauty</h1>
                 <p className="text-4xl text-pink-600 mb-8 font-semibold">Enhancing Your Look with Professional Makeup Services</p>
                 <Link href="#services">
