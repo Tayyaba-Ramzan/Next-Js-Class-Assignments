e94426e12b1d774db988aa1eaa1bf1515b8488e7
Tayyaba Ramzan <160269220+tayyaba-ramzan@users.noreply.github.com>
2024-10-19 11:46:53 -0700

    Update HeroSection.tsx

--- assignment-03/src/components/HeroSection.tsx
+++ assignment-03/src/components/HeroSection.tsx
@@ -12,7 +12,7 @@ const HeroSection = () => {
             <div
             className="text-center">
                 <h1 className="text-6xl font-bold text-pink-600 mb-4">Unleash Your Beauty</h1>
-                <p className="text-4xl text-pink-600 mb-8 font-semibold">Enhancing Your Look with Professional Makeup Services</p>
+                <p className="text-4xl text-pink-600 mb-8 font-semibold mx-20">Enhancing Your Look with Professional Makeup Services</p>
                 <Link href="#services">
                     <button className="bg-pink-600 hover:bg-pink-700 text-white py-2 px-4 rounded-lg transition duration-300">
                         Explore Services
