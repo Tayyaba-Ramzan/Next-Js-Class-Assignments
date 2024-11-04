07a549ffdf543c42977e52baf71baa95df0f2b92
Tayyaba Ramzan <160269220+Tayyaba-Ramzan@users.noreply.github.com>
2024-11-03 23:19:28 -0800

    Add responsive Hero Section for XL screens

---
+++ assignment-04/src/app/page.tsx
@@ -0,0 +1,15 @@
+import HeroSection from '@/components/HeroSection'
+import Navbar from '@/components/Navbar'
+import React from 'react'
+
+const HomePage = () => {
+  return (
+    <>
+    <Navbar/>
+    <HeroSection/>
+
+    </>
+  )
+}
+
+export default HomePage
